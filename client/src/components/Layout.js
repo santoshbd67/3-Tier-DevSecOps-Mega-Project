091d02646aa5cf89a8510a09b3399a76d76e5d24
Santosh Dabbannavar <150413783+santoshbd67@users.noreply.github.com>
2025-07-16 12:18:13 +0530

    Update Layout.js

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -16,14 +16,14 @@ function Layout({ children }) {
           </div>
         </div>
       </header>
-      <AnimatedBanner message="Welcome to DevOps Shack 🚀" />
+      <AnimatedBanner message="Welcome to Clouddnyana 🚀" />
       <div className="app-body">
         <aside className="sidebar slide-in-left">
           <h3>Connect</h3>
           <ul className="social-links">
-            <li><a className="sidebar-btn" href="https://www.linkedin.com/in/adityajaiswal7/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+            <li><a className="sidebar-btn" href="https://www.linkedin.com/in/santosh-dabbannavar/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
             <li><a className="sidebar-btn" href="https://www.youtube.com/@devopsshack" target="_blank" rel="noopener noreferrer">YouTube</a></li>
-            <li><a className="sidebar-btn" href="https://www.instagram.com/devopsshack" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+            <li><a className="sidebar-btn" href="https://www.instagram.com/janatha_raj/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
           </ul>
         </aside>
         <main className="main-content fade-in">
